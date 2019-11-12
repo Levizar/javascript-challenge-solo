bbb7c55ed2186a1f252586dd372c5c2b061fc4c9
Brice Levizar Bartoletti <bartolettibrice@gmail.com>
2019-11-12 17:04:30 +0100

    try to get data

--- assets/becode-app.js
+++ assets/becode-app.js
@@ -1,10 +1,26 @@
 /* 
 // WRITE YOUR JAVASCRIPT BELOW THIS COMMENT 
-Your name :     
-Date :  
-Contact information : 
+Your name : Bartoletti Brice
+Date : 12/11/2019
+Contact information :  
 What does this script do ? 
 ...
 */
 
-// Your scripting goes here...
+// Récupération des données
+
+let data = [];
+
+let tableLines = d3.selectAll("#table1 > tbody:nth-child(3) > tr")
+
+tableLines.nodes().forEach(element => {
+    
+});
+
+console.log(tableLines);
+
+
+
+
+
+

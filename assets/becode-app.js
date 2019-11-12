732f467f881ee973d59c1dd60c42fe88b9ae260c
Brice Bartoletti <bartolettibrice@gmail.com>
2019-11-12 21:54:00 +0100

    HTML DATA ok

--- assets/becode-app.js
+++ assets/becode-app.js
@@ -7,20 +7,40 @@ What does this script do ?
 ...
 */
 
-// Récupération des données
+// This function get datas from tables from an HTML using the D3 selector
+const getDataFromHTMLTable = (stringCSSPathToTheTableRows) => {
+    let data = [];
+    // getting the row of the table in an object an taking out the nodes, turning them into an arrayLike
+    let tableRows = d3.selectAll(stringCSSPathToTheTableRows);
+    tableRows = [...tableRows.nodes()];
+    let tableHeaders = [];
+    for( let i = 0; i < tableRows.length; i++){
+        const arrCellsOfCurrentRow = [...tableRows[i].cells];
+        let countryData = [];
+        for( let j = 1; j < arrCellsOfCurrentRow.length; j++){
+            if( i == 0){
+                if(j == 1){
+                    tableHeaders[j-1] = "country";
+                }else{
+                    tableHeaders[j-1] = arrCellsOfCurrentRow[j].innerText;
+                }
+            } else {
+                countryData[j-1] = arrCellsOfCurrentRow[j].innerText
+            }
+        }
+        if( i != 0){
+            data[i-1] = Object.fromEntries(tableHeaders.map((a,i) => [tableHeaders[i], countryData[i]]))
+        }
+        // console.log(countryData);
+    }
+    return data;
+}
 
-let data = [];
-
-let tableLines = d3.selectAll("#table1 > tbody:nth-child(3) > tr")
-
-tableLines.nodes().forEach(element => {
-    
-});
-
-console.log(tableLines);
+let dataTableOne = getDataFromHTMLTable("#table1 > tbody:nth-child(3) > tr")
+console.log(dataTableOne);
 
 
 
 
-
-
+let dataTableTwo = getDataFromHTMLTable("#table2 tr")
+console.log(dataTableTwo);

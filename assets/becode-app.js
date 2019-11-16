67b472b0d4f364f4920b15f433a9676367601aa9
Brice Bartoletti <bartolettibrice@gmail.com>
2019-11-16 18:14:48 +0100

    simple code format + comments

--- assets/becode-app.js
+++ assets/becode-app.js
@@ -27,7 +27,7 @@ const getDataFromHTMLTable = (stringCSSPathToTheTableRows) => {
                 }
             } else {                                                                                        // Next iterations to get the datas
                 countryData[j - 1] = arrCellsOfCurrentRow[j].innerText;
-                countryData[j - 1] = countryData[j - 1].replace(/[,]/g, '.')     ;                           // replacing "," by "." for calculation
+                countryData[j - 1] = countryData[j - 1].replace(/[,]/g, '.');                           // replacing "," by "." for calculation
             }
         }
         if (i != 0) {                                                                                       // 
@@ -70,89 +70,79 @@ const parseToLineData = arrayOfCountry => {
         }
 
         for (let j = parsedObjCountry.data.length - 1; j >= 0; j--) {
-            parsedObjCountry.data[j].value = parsedObjCountry.data[j].value / parsedObjCountry.data[0].value;           
+            parsedObjCountry.data[j].value = parsedObjCountry.data[j].value / parsedObjCountry.data[0].value;
         }
         parsedLineData.push(parsedObjCountry);
     }
     return parsedLineData;
 }
 
-// data from table One
-let dataTableOne = getDataFromHTMLTable("#table1 > tbody:nth-child(3) > tr");
-dataTableOne = parseToLineData(dataTableOne);
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////                                                                                                  //////////////
+//////////////                                  LINE CHART FUNCTION                                             //////////////
+//////////////                                                                                                  //////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Line charts
-//Make a line chart
-const lineChart = (dataSet)=>{
+const lineChart = (dataSet) => {
     // Defining the chart default param
     const width = 800;
-    const height = width/2;
+    const height = width / 2;
     const margin = {
-        left : 25,
-        bottom : 20,
-        right : 60,
-        top : 20,
-      }
+        left: 25,
+        bottom: 20,
+        right: 60,
+        top: 20,
+    }
     const chartWidth = width - margin.left - margin.right;
     const chartHeight = height - margin.top - margin.bottom;
 
     // Inserting the SVG Canvas in the html
     const svg = d3.select("#mw-content-text").insert(`svg`, "#table1").attr("id", "SVGTable1").attr("width", `${width}`).attr("height", `${height}`);
-    
+
     // Inserting a button for data filter
     d3.select("#mw-content-text")
-    .insert("select", "#SVGTable1")
-    .attr("id", "selectButton")
-    .style("margin-top", "15px")
-    .style("margin-bottom", "15px")
-    .selectAll('myOptions')
-    .data(dataSet)
-    .enter()
-    .append('option')
-    .text( d => d.key ) // text showed in the menu
-    .attr("value", d => d.key ) // corresponding value returned by the button
-    
+        .insert("select", "#SVGTable1")
+        .attr("id", "selectButton")
+        .style("margin-top", "15px")
+        .style("margin-bottom", "15px")
+        .selectAll('myOptions')
+        .data(dataSet)
+        .enter()
+        .append('option')
+        .text(d => d.key) // text showed in the menu
+        .attr("value", d => d.key) // corresponding value returned by the button
+
     // filter the data to initialize the chart with the first option of the select button
     let choosenCountry = d3.select("#selectButton").property("value")
-    let lineData = dataSet.filter( d => d.key == choosenCountry)
+    let lineData = dataSet.filter(d => d.key == choosenCountry)
 
-    // Defining the chart scale with default "wrong" value. It is updated at the end of the function with right value
+    // Defining the chart scale. The value will be given in the update function
     let xScale = d3.scaleLinear()
-    // .domain([2002, 2012])                                                           
-    // .range([0, chartWidth]);
-    
-    let yScale = d3.scaleLinear()                                                 
-    // .domain([0, 4])
-    // .range([chartHeight, 0]);
+    let yScale = d3.scaleLinear()
 
-    // Define the scaling for the scale
-    let xAxisGenerator = d3.axisBottom(xScale)                                    
-    // .tickValues(d3.range(2002, 2013, 1));
-
-    let yAxisGenerator = d3.axisLeft(yScale)                                      
-    // .tickValues(d3.range(0, 4.1, 0.1));
+    // initialiazing the scaling for the scale, .tickvalue will be added in update function
+    let xAxisGenerator = d3.axisBottom(xScale)
+    let yAxisGenerator = d3.axisLeft(yScale)
 
     // Creating a line generator function
     let lineGenerator = d3.line()
-                    .x(d => xScale(d.date))
-                    .y(d => yScale(d.value));
+        .x(d => xScale(d.date))
+        .y(d => yScale(d.value));
 
     // Function to get the last index of an array to get his position (see valueLabel)
     const last = array => array[array.length - 1]
 
-
-
-    // Defining the scale and drawing area
+    // Defining the scale and drawing area inside the svg
     const g = svg.append("g")
         .attr("transform", `translate(${margin.left}, ${margin.top})`);
-    
-    // Getting the X Axis and putting it at the bottom (the max Y position from top to down)
+
+    // Initialiazing the axes, they would be redefined in the update function
     let xAxis = g.append("g")
         .call(xAxisGenerator)
         .attr("transform", `translate(0, ${chartHeight})`);
-
-    // Getting the Y axis at the 0 from g group
     let yAxis = g.append("g")
         .call(yAxisGenerator);
 
@@ -164,78 +154,78 @@ const lineChart = (dataSet)=>{
     const valueLabel = g
         .append("g")
         .data(lineData)
-        // .attr("transform", d => `translate(${xScale(last(d.data).date)}, ${yScale(last(d.data).value)})`);
-        
+        .attr("transform", d => `translate(${xScale(last(d.data).date)}, ${yScale(last(d.data).value)})`);
+
     const circle = valueLabel.append("circle")
-    .attr("r", 3)
-    .style("stroke", "white")
-    .style("fill", d => d.light);
+        .attr("r", 3)
+        .style("stroke", "white")
+        .style("fill", d => d.light);
 
     const countryName = valueLabel.append("text")
-    .text(d => d.key)
-    .attr("dy", -15)
-    .attr("dx", -20)
-    .style("font-family", "monospace")
-    .style("fill", d => d.light);
+        .text(d => d.key)
+        .attr("dy", -15)
+        .attr("dx", -20)
+        .style("font-family", "monospace")
+        .style("fill", d => d.light);
 
     const update = () => {
         choosenCountry = d3.select("#selectButton").property("value")
-        lineData = dataSet.filter( d => d.key == choosenCountry)
+        lineData = dataSet.filter(d => d.key == choosenCountry)
 
         let data = lineData[0].data
         let valueArray = []
         data.forEach(datacouple => valueArray.push(datacouple.value));
-        let maxYaxis = Math.ceil(d3.max(valueArray)*10)/10
-            
+        let maxYaxis = Math.ceil(d3.max(valueArray) * 10) / 10
+
         xScale = d3.scaleLinear()
-        .domain([data[0].date, last(data).date])                                                           
-        .range([0, chartWidth]);
-        
-        yScale = d3.scaleLinear()                                                 
-        .domain([0, maxYaxis]) //
-        .range([chartHeight, 0]);
-        
+            .domain([data[0].date, last(data).date])
+            .range([0, chartWidth]);
+
+        yScale = d3.scaleLinear()
+            .domain([0, maxYaxis]) //
+            .range([chartHeight, 0]);
+
         let formatter = d3.format(".0%")                             // make percentage in tick formater
 
-        xAxisGenerator = d3.axisBottom(xScale)                                    
-        .tickValues(d3.range(data[0].date, last(data).date +1, 1));
-    
-        yAxisGenerator = d3.axisLeft(yScale)                                      
-        .tickValues(d3.range(0, maxYaxis +0.1, 0.1))
+        xAxisGenerator = d3.axisBottom(xScale)
+            .tickValues(d3.range(data[0].date, last(data).date + 1, 1));
+
+        yAxisGenerator = d3.axisLeft(yScale)
+            .tickValues(d3.range(0, maxYaxis + 0.1, 0.1))
         // .tickFormat(formatter);                                   // need to modify
 
         xAxis.call(xAxisGenerator)
 
         yAxis.call(yAxisGenerator);
 
-        line 
-        .data(lineData)
-        .transition()
-        .duration(1000)
-        .attr("d", d => lineGenerator(d.data))                                      // lineGenerator: d3.line().attr(X).attr(Y)
-        .style("fill", "none")
-        .style("stroke", d => d.light)
-        .style("stroke-width", 2)
-        .style("stroke-linejoin", "round");
+        line
+            .data(lineData)
+            .transition()
+            .duration(1000)
+            .attr("d", d => lineGenerator(d.data))                                      // lineGenerator: d3.line().attr(X).attr(Y)
+            .style("fill", "none")
+            .style("stroke", d => d.light)
+            .style("stroke-width", 2)
+            .style("stroke-linejoin", "round");
 
         valueLabel
-        .data(lineData)
-        .transition()
-        .duration(1000)
-        .attr("transform", d => `translate(${xScale(last(d.data).date)}, ${yScale(last(d.data).value)})`);
+            .data(lineData)
+            .transition()
+            .duration(1000)
+            .attr("transform", d => `translate(${xScale(last(d.data).date)}, ${yScale(last(d.data).value)})`);
 
         circle
-        .data(lineData)
-        .transition()
-        .duration(1000)
-        .style("fill", d => d.light);
+            .data(lineData)
+            .transition()
+            .duration(1000)
+            .style("fill", d => d.light);
 
         countryName
-        .data(lineData)
-        .transition()
-        .duration(1000)
-        .text(d => d.key)
-        .style("fill", d => d.light)
+            .data(lineData)
+            .transition()
+            .duration(1000)
+            .text(d => d.key)
+            .style("fill", d => d.light)
     }
     update()
 
@@ -243,20 +233,11 @@ const lineChart = (dataSet)=>{
 
 }
 
+// Calling the line chart function for the first table data
+
+let dataTableOne = getDataFromHTMLTable("#table1 > tbody:nth-child(3) > tr");
+dataTableOne = parseToLineData(dataTableOne);
 lineChart(dataTableOne)
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -269,8 +250,3 @@ lineChart(dataTableOne)
 // let dataTableTwo = getDataFromHTMLTable("#table2 tr")
 // console.log(dataTableTwo);
 
-// Note: pour le graphique, le contenu du texte indique que ce qui compte est l'évolution des données dans le temps.
-// Il faut donc créer une courbe par pays en fonction du temps que l'on superposera sur un graphique
-// Il est à noter que vu que c'est l'évolution qui est évaluée et non les valeurs absolues
-// Il est plus pertinent de formater les données en les rendant relatives, ce qui élimine le biais des quantités d'habitants.
-// donnée relative x = Donnée x / donnée initiale

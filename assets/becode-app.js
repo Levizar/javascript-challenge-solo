fab35008642032c5cbdb0380d59851d3bc48b2ad
Brice Bartoletti <bartolettibrice@gmail.com>
2019-11-16 18:00:55 +0100

    first chart nearly done

--- assets/becode-app.js
+++ assets/becode-app.js
@@ -92,113 +92,161 @@ const lineChart = (dataSet)=>{
         left : 25,
         bottom : 20,
         right : 60,
-        top : 10,
+        top : 20,
       }
     const chartWidth = width - margin.left - margin.right;
     const chartHeight = height - margin.top - margin.bottom;
+
+    // Inserting the SVG Canvas in the html
+    const svg = d3.select("#mw-content-text").insert(`svg`, "#table1").attr("id", "SVGTable1").attr("width", `${width}`).attr("height", `${height}`);
     
-    // Defining the chart scale
-    const xScale = d3.scaleLinear()
-    .domain([2002, 2012])                                                           // To modify
-    .range([0, chartWidth]);
+    // Inserting a button for data filter
+    d3.select("#mw-content-text")
+    .insert("select", "#SVGTable1")
+    .attr("id", "selectButton")
+    .style("margin-top", "15px")
+    .style("margin-bottom", "15px")
+    .selectAll('myOptions')
+    .data(dataSet)
+    .enter()
+    .append('option')
+    .text( d => d.key ) // text showed in the menu
+    .attr("value", d => d.key ) // corresponding value returned by the button
     
-    const yScale = d3.scaleLinear()                                                 // To modify
-    .domain([0, 2])
-    .range([chartHeight, 0]);
+    // filter the data to initialize the chart with the first option of the select button
+    let choosenCountry = d3.select("#selectButton").property("value")
+    let lineData = dataSet.filter( d => d.key == choosenCountry)
+
+    // Defining the chart scale with default "wrong" value. It is updated at the end of the function with right value
+    let xScale = d3.scaleLinear()
+    // .domain([2002, 2012])                                                           
+    // .range([0, chartWidth]);
+    
+    let yScale = d3.scaleLinear()                                                 
+    // .domain([0, 4])
+    // .range([chartHeight, 0]);
 
     // Define the scaling for the scale
-    const xAxisGenerator = d3.axisBottom(xScale)                                    // To modify
-    .tickValues(d3.range(2002, 2013, 1));
+    let xAxisGenerator = d3.axisBottom(xScale)                                    
+    // .tickValues(d3.range(2002, 2013, 1));
 
-    const yAxisGenerator = d3.axisLeft(yScale)                                      // To modify
-    .tickValues(d3.range(0, 2.1, 0.1));
+    let yAxisGenerator = d3.axisLeft(yScale)                                      
+    // .tickValues(d3.range(0, 4.1, 0.1));
 
     // Creating a line generator function
-    const lineGenerator = d3.line()
+    let lineGenerator = d3.line()
                     .x(d => xScale(d.date))
                     .y(d => yScale(d.value));
 
     // Function to get the last index of an array to get his position (see valueLabel)
     const last = array => array[array.length - 1]
 
-    // Inserting the SVG Canvas in the html
-    const svg = d3.select("#mw-content-text").insert(`svg`, "#table1").attr("id", "SVGTable1").attr("width", `${width}`).attr("height", `${height}`);
-    
-    let lineData = dataSet;
-    // Inserting a button for data filter
-    d3.select("#mw-content-text")
-      .insert("select", "#SVGTable1")
-      .attr("id", "#selectButton")
-      .style("margin-top", "15px")
-      .style("margin-bottom", "15px")
-        .selectAll('myOptions')
-     	.data(lineData)
-        .enter()
-    	.append('option')
-        .text( d => d.key ) // text showed in the menu
-        .attr("value", d => d ) // corresponding value returned by the button
-    
-        // let lineData = d3.select("#selectButton").value;
-        // console.log(lineData)
 
 
-        // Defining the scale and drawing area
+    // Defining the scale and drawing area
     const g = svg.append("g")
         .attr("transform", `translate(${margin.left}, ${margin.top})`);
     
     // Getting the X Axis and putting it at the bottom (the max Y position from top to down)
-    g.append("g")
+    let xAxis = g.append("g")
         .call(xAxisGenerator)
         .attr("transform", `translate(0, ${chartHeight})`);
 
     // Getting the Y axis at the 0 from g group
-    g.append("g")
+    let yAxis = g.append("g")
         .call(yAxisGenerator);
 
-    // Defining the line(s)
-    g.selectAll(".line")
-        .data(lineData)
-        .enter()
+    // Defining one line, its data will be updated at the end of the function
+    let line = g
         .append("path")
+
+    // Creating a g group at the position of the last point of the chart
+    const valueLabel = g
+        .append("g")
+        .data(lineData)
+        // .attr("transform", d => `translate(${xScale(last(d.data).date)}, ${yScale(last(d.data).value)})`);
+        
+    const circle = valueLabel.append("circle")
+    .attr("r", 3)
+    .style("stroke", "white")
+    .style("fill", d => d.light);
+
+    const countryName = valueLabel.append("text")
+    .text(d => d.key)
+    .attr("dy", -15)
+    .attr("dx", -20)
+    .style("font-family", "monospace")
+    .style("fill", d => d.light);
+
+    const update = () => {
+        choosenCountry = d3.select("#selectButton").property("value")
+        lineData = dataSet.filter( d => d.key == choosenCountry)
+
+        let data = lineData[0].data
+        let valueArray = []
+        data.forEach(datacouple => valueArray.push(datacouple.value));
+        let maxYaxis = Math.ceil(d3.max(valueArray)*10)/10
+            
+        xScale = d3.scaleLinear()
+        .domain([data[0].date, last(data).date])                                                           
+        .range([0, chartWidth]);
+        
+        yScale = d3.scaleLinear()                                                 
+        .domain([0, maxYaxis]) //
+        .range([chartHeight, 0]);
+        
+        let formatter = d3.format(".0%")                             // make percentage in tick formater
+
+        xAxisGenerator = d3.axisBottom(xScale)                                    
+        .tickValues(d3.range(data[0].date, last(data).date +1, 1));
+    
+        yAxisGenerator = d3.axisLeft(yScale)                                      
+        .tickValues(d3.range(0, maxYaxis +0.1, 0.1))
+        // .tickFormat(formatter);                                   // need to modify
+
+        xAxis.call(xAxisGenerator)
+
+        yAxis.call(yAxisGenerator);
+
+        line 
+        .data(lineData)
+        .transition()
+        .duration(1000)
         .attr("d", d => lineGenerator(d.data))                                      // lineGenerator: d3.line().attr(X).attr(Y)
         .style("fill", "none")
         .style("stroke", d => d.light)
         .style("stroke-width", 2)
         .style("stroke-linejoin", "round");
 
-    // Creating a g group at the position of the last point of the chart
-    const valueLabel = g.selectAll(".label")
+        valueLabel
         .data(lineData)
-        .enter().append("g")
+        .transition()
+        .duration(1000)
         .attr("transform", d => `translate(${xScale(last(d.data).date)}, ${yScale(last(d.data).value)})`);
-        
-    valueLabel.append("circle")
-    .attr("r", 3)
-    .style("stroke", "white")
-    .style("fill", d => d.light);
 
-    valueLabel.append("text")
-    .text(d => d.key)
-    .attr("dy", 5)
-    .attr("dx", 10)
-    .style("font-family", "monospace")
-    .style("fill", d => d.light);
+        circle
+        .data(lineData)
+        .transition()
+        .duration(1000)
+        .style("fill", d => d.light);
 
-    // Return the node so it can be stored in a variable
-    return svg.node() 
+        countryName
+        .data(lineData)
+        .transition()
+        .duration(1000)
+        .text(d => d.key)
+        .style("fill", d => d.light)
+    }
+    update()
+
+    d3.select("#selectButton").on("change", update)
+
 }
 
 lineChart(dataTableOne)
 
 
 
-// sert à check les valeurs déviant fortement
-// dataTableOne.forEach( obj =>{
-//     obj.data.forEach(objdata => {
-//        objdata.value > 2 ? console.log(obj.key) : 0
-//     });
-// })
-console.log(dataTableOne)
 
 
 
